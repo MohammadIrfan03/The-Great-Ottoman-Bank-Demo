799a13410405cf0ba22e77519836bf77b51b6564
Mohammad Irfan <ashrafirfan021@gmail.com>
2026-02-01 00:46:15 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,43 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'ottoman123',
+  database: 'ottoman_bank'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+    if (results.length > 0) {
+      res.send('Login successful');
+    } else {
+      res.send('Invalid credentials');
+    }
+  });
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], err => {
+    if (err) throw err;
+    res.send('User registered');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
