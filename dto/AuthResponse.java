5d51e8d4ff73c1c361f8dd964944b9da34ddf718
Mohammad Irfan <ashrafirfan021@gmail.com>
2026-09-06 05:33:44 +0530

    Update AuthResponse.java

--- dto/AuthResponse.java
+++ dto/AuthResponse.java
@@ -15,15 +15,3 @@ public class AuthResponse {
     private String email;
     private String role;
 }
-repository/UserRepository.java
-package com.ottomanbank.authservice.repository;
-
-import com.ottomanbank.authservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
-}
